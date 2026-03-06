15cf57fff74692d1e92d093b8912ea911026fcec
kriskizlyk <kriskizlyk@gmail.com>
2026-03-06 11:04:59 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mylists-v10';
+const CACHE_NAME = 'mylists-v13';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
@@ -6,12 +6,15 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
+  // Skip non-GET requests (API calls, etc)
+  if (e.request.method !== 'GET') return;
+
   e.respondWith(
     fetch(e.request).then(res => {
       if (res.status === 200) {
